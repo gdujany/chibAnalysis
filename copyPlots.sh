267ee34f228648710a928b9f4f499dad200bc564
Giulio Dujany <giulio.dujany@yahoo.it>
2013-10-03 15:09:52 +0100

    modified copyPlots.sh

--- copyPlots.sh
+++ copyPlots.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-cp plots/*.pdf  ../tesi/myDir/notes/AN-13-194/trunk/figures/plotsChib
-cp plots/*.root ../tesi/myDir/notes/AN-13-194/trunk/figures/plotsChib
+cp plots/*.pdf  ../tesi/myDir/notes/AN-13-194/trunk/figures-withGammaPtCut/plotsChib
+cp plots/*.root ../tesi/myDir/notes/AN-13-194/trunk/figures-withGammaPtCut/plotsChib
 
-cp AgreementDataMC/plots/*S*.pdf ../tesi/myDir/notes/AN-13-194/trunk/figures/data_MC_agreement/
-cp AgreementDataMC/plots/*S*.root ../tesi/myDir/notes/AN-13-194/trunk/figures/data_MC_agreement/
+cp AgreementDataMC/plots/*S*.pdf ../tesi/myDir/notes/AN-13-194/trunk/figures-withGammaPtCut/data_MC_agreement/
+cp AgreementDataMC/plots/*S*.root ../tesi/myDir/notes/AN-13-194/trunk/figures-withGammaPtCut/data_MC_agreement/
 
-cp CBParamsPlots/plots/* ../tesi/myDir/notes/AN-13-194/trunk/figures/DSCB_parameters/
+cp CBParamsPlots/plots/* ../tesi/myDir/notes/AN-13-194/trunk/figures-withGammaPtCut/DSCB_parameters/
 
-cp provaVariCuts/imgPU/refit_PU* ../tesi/myDir/notes/AN-13-194/trunk/figures/PU/
+cp provaVariCuts/imgPU/refit_PU* ../tesi/myDir/notes/AN-13-194/trunk/figures-withGammaPtCut/PU/
 
-cp sistematici/plotsVariaCB/ratios_refit* ../tesi/myDir/notes/AN-13-194/trunk/figures/sistematici/
-cp sistematici/plotsVariaFunz/*.pdf ../tesi/myDir/notes/AN-13-194/trunk/figures/sistematici/
-cp sistematici/plotsVariaFunz/*.root ../tesi/myDir/notes/AN-13-194/trunk/figures/sistematici/
+cp sistematici/plotsVariaCB/ratios_refit* ../tesi/myDir/notes/AN-13-194/trunk/figures-withGammaPtCut/sistematici/
+cp sistematici/plotsVariaFunz/*.pdf ../tesi/myDir/notes/AN-13-194/trunk/figures-withGammaPtCut/sistematici/
+cp sistematici/plotsVariaFunz/*.root ../tesi/myDir/notes/AN-13-194/trunk/figures-withGammaPtCut/sistematici/
 
-cp effHistos/plots/plotsEff_hx_Upsilon2SPt_2S_refit_*.pdf ../tesi/myDir/notes/AN-13-194/trunk/figures/Efficiency/
-cp effHistos/plots/plotsEff_hx_Upsilon2SPt_2S_refit_*.root ../tesi/myDir/notes/AN-13-194/trunk/figures/Efficiency/
+cp effHistos/plots/plotsEff_hx_Upsilon2SPt_2S_refit_*.pdf ../tesi/myDir/notes/AN-13-194/trunk/figures-withGammaPtCut/Efficiency/
+cp effHistos/plots/plotsEff_hx_Upsilon2SPt_2S_refit_*.root ../tesi/myDir/notes/AN-13-194/trunk/figures-withGammaPtCut/Efficiency/
 
-cp tables/*.tex ../tesi/myDir/notes/AN-13-194/trunk/tables/
+cp tables/*.tex ../tesi/myDir/notes/AN-13-194/trunk/tables-withGammaPtCut/
+cp tables/*.pdf ../tesi/myDir/notes/AN-13-194/trunk/tables-withGammaPtCut/
 
